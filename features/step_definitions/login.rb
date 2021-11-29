de6aa9cf0e8b43fcacbfb843d8d1237faa3e30f5
dussan <dussan.freire@ucb.edu.bo>
2021-11-29 17:48:55 -0400

    Se agrego la base para el login

--- features/step_definitions/login.rb
+++ features/step_definitions/login.rb
@@ -1,27 +1,32 @@
-Given('I am on the Nahual homepage') do
-  pending # Write code here that turns the phrase above into concrete actions
+Given("I am on the Nahual homepage") do
+	page.driver.browser.manage.window.maximize
+        visit 'https://nahual-argentina-develop.vercel.app/'
 end
 
-When('I click the Iniciar Sesión button') do
-  pending # Write code here that turns the phrase above into concrete actions
+Given('I click the Iniciar Sesión button') do
+    click_on ('Iniciar Sesión')
+end
+  
+Given('I click on the Iniciar con Google button') do
+    find(:xpath, "/html/body/div/div/div[2]/form/div/div/div/div/div[2]/div[2]/span/div/div/div/div/div/div/div/div/div/div/div[1]/a/div[2]").click
 end
 
-When('I click on the Iniciar con Google button') do
-  pending # Write code here that turns the phrase above into concrete actions
+Given('I add my email: calidad.nahual@yahoo.com') do
+    fill_in 'identifierId', :with => 'calidad.nahual@yahoo.com'
+end
+Given('I click on the Siguiente button') do
+    xpathBase= find(:xpath, "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/span").click
 end
 
-When('I add my email: john.doe.ucbcba@gmail.com') do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When('I add my password: calidad2021') do
-  pending # Write code here that turns the phrase above into concrete actions
+Given('I add my password: calidad2021') do
+    xpathBase= find(:xpath, "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input").
+    xpathBase.set('calidad2021')
 end
 
 When('I click on the button Siguiente') do
-  pending # Write code here that turns the phrase above into concrete actions
+    xpathBase= find(:xpath, "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/span").click
 end
 
 Then('would be logged') do
-  pending # Write code here that turns the phrase above into concrete actions
+    puts('jajaj')
 end
